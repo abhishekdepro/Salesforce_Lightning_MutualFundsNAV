7727019e307402e219636b28def4cab6f338494d
Abhishek Dey <abhishekde@hotmail.com>
2018-04-10 23:49:45 +0530

    Modified Lightning code

--- src/aura/NAV_Details/NAV_DetailsHelper.js
+++ src/aura/NAV_Details/NAV_DetailsHelper.js
@@ -7,6 +7,16 @@
             if (state === "SUCCESS") {
                 console.log('Result..'+JSON.stringify(response.getReturnValue()));
                 component.set("v.myFunds",response.getReturnValue());
+                var myFunds = component.get("v.myFunds");
+                for(var i=0;i<myFunds.length;i++){
+                    if(myFunds[i].Percentage_Change__c<0){
+                    	myFunds[i].isNegative__c = true; 
+                    }else{
+                     	myFunds[i].isNegative__c = false;    
+                    } 
+                }
+                component.set("v.myFunds",myFunds);
+                console.log('myFunds...'+JSON.stringify(myFunds));
             }
         });
         $A.enqueueAction(action);
